2229c35953272820dd8cff8c4408473e2634f9ee
Vinayak <vinayak.tripathi2025@vitstudent.ac.in>
2026-06-17 23:25:56 +0530

    fix: Make landing page dynamic to show live stats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { TextReveal } from "@/components/ui/cascade-text";
 import connectDB from "@/lib/mongodb";
 import Task from "@/models/Tasks";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   await connectDB();
 
